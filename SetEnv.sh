5725fa2da81e597132560a5da7eedb96df00b5b5
mbongale <45281828+mbongale@users.noreply.github.com>
2023-01-23 10:48:20 +0530

    Update SetEnv.sh

--- SetEnv.sh
+++ SetEnv.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+mkdir -p /opt/gradle
+wget https://services.gradle.org/distributions/gradle-7.4-bin.zip
+unzip -d /opt/gradle gradle-7.4-bin.zip
+export GRADLE_HOME=/opt/gradle/gradle-7.4
+export PATH=${GRADLE_HOME}/bin:${PATH}
+gradle -v
+apt install android-sdk - y
+apt install sdkmanager -y
+cp -R /usr/bin sdkmanager /usr/lib/android-sdk/tools/bin
+export ANDROID_HOME=/usr/lib/android-sdk/tools/bin
+yes | /usr/lib/android-sdk/tools/bin/sdkmanager --licenses
